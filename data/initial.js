baa8424b12db6c2d452b22b30e3972fbcabd00f6
akshittyrannix <akshit@tyrannix.com>
2023-07-13 10:25:17 +0530

    initial data stored on mongoDB cluster

--- data/initial.js
+++ data/initial.js
@@ -1,3 +1,22 @@
 require("dotenv").config();
 
 const connectDB = require("../db/connect");
+const Product = require("../models/products");
+
+const productsData = require("./products.json");
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    await Product.deleteMany();
+    await Product.create(productsData);
+    console.log("Products data created successfully");
+    process.exit(0);
+  } catch (error) {
+    console.log("Error creating products data");
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+start();
